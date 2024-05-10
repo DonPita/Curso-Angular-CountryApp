4d4b8b2a1c077830a2bec604ba7cd626c42c99dc
Adrian Pita <pitabermudez@gmail.com>
2024-05-10 09:34:23 +0200

    Modificaciones para mantener la información en las 3 diferentes busquedas

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
 
@@ -7,18 +7,26 @@ import { Country } from '../../interfaces/country.interface';
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnInit {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public initialValue: string = '';
 
   constructor(private countriesSevice: CountriesService) { }
 
-  searchByCapital(term: string):void {
+  //Para guardar la info al cambiar de página
+  ngOnInit(): void {
+    this.countries = this.countriesSevice.cacheStore.byCapital.countries;
+    this.initialValue = this.countriesSevice.cacheStore.byCapital.term;
+  }
+
+  searchByCapital(term: string): void {
+
     this.isLoading = true;
 
     this.countriesSevice.searchCapital(term)
-      .subscribe( countries => { //si no te suscribes no funciona
+      .subscribe(countries => { //si no te suscribes no funciona
         this.countries = countries;
         this.isLoading = false;
       });
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, delay, map, of, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+
 import { Country } from '../interfaces/country.interface';
 import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
 
 
 @Injectable({ providedIn: 'root' })
@@ -28,7 +30,7 @@ export class CountriesService {
       )
   }
 
-
+  //Para la country-page, buscar por el codigo
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url = `${this.apiUrl}/alpha/${code}`;
     return this.http.get<Country[]>(url)
@@ -42,21 +44,27 @@ export class CountriesService {
   /*El retorno es un Observable, de un arreglo de paises, por tanto hay que
   decirselo al get tambien, si no da error*/
   searchCapital(term: string): Observable<Country[]> {
-    //Mas fácil de leer de esta manera el codigo de abajo
     const url = `${this.apiUrl}/capital/${term}`;
     return this.getCountriesRequest(url)
       .pipe(
+        //Tap desata eventos secundarios, que no afectan al desarrollo normal.
         tap(countries => this.cacheStore.byCapital = { term, countries })
       );
   }
 
   searchCountry(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/translation/${term}`;
-    return this.getCountriesRequest(url);
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap(countries => this.cacheStore.byCountries = { term, countries })
+      );
   }
 
-  searchRegion(region: string): Observable<Country[]> {
+  searchRegion(region: Region): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${region}`;
-    return this.getCountriesRequest(url);
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap(countries => this.cacheStore.byRegion = { region, countries })
+      );
   }
 }
