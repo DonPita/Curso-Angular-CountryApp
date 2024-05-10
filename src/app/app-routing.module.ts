a3896f9cf5c787606eddd576c1330b584f874d6f
Adrian Pita <pitabermudez@gmail.com>
2024-05-10 10:33:51 +0200

    Aplicacion en Local funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,6 @@ const routes: Routes = [
   },
   {
     path: '**',
-    component: ByCountryPageComponent,
     redirectTo: 'countries'
   }
 ];
--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -39,7 +39,6 @@ const routes: Routes = [
     RouterModule
   ],
 
-
 })
 
 export class CountriesRoutingModule { }
